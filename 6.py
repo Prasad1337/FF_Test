592d77cf50a9dfb44e8634f1a8d78121ba849a1b
Prasad <prasad.kamat@build.com>
2017-05-16 22:46:20 -0700

    staging point

--- 6.py
+++ 6.py
@@ -1,43 +1,43 @@
-import Tkinter
 import json
+from Tkinter import *
 
 
 # UI Class
 class CityInformation:
     # Constructor
-    def __init__(self, master):
-        city_info_file = open("ca.json", "r")
-        
+    def __init__(self, master, file_name):
+        city_info_file = open(file_name, "r")
+        city_info = json.loads(city_info_file.read())
+        city_info_file.close()
+
+        options = []
+        for x in city_info:
+            options.append(x['name'])
+        options.sort()
+
+        variable = StringVar(master)
+        variable.set(options[0])  # default value
+        city_picker = apply(OptionMenu, (master, variable) + tuple(options))
 
         master.wm_title("City Information")
-        left_frame = Tkinter.Frame(master)
-        right_frame = Tkinter.Frame(master)
-        left_frame.pack(side=Tkinter.LEFT)
-        right_frame.pack(side=Tkinter.RIGHT)
+        city_label = Label(master, text="City")
+        county_label = Label(master, text="County")
+        latitude_label = Label(master, text="Latitude")
+        longitude_label = Label(master, text="Longitude")
+        county_value = Entry(master, width=40)
+        latitude_value = Entry(master, width=40)
+        longitude_values = Entry(master, width=40)
 
-        variable = Tkinter.StringVar(master)
-        variable.set("")  # default value
-
-        city_label = Tkinter.Label(left_frame, text="City", anchor="w", width=10)
-        county_label = Tkinter.Label(left_frame, text="County", anchor="w", width=10)
-        latitude_label = Tkinter.Label(left_frame, text="Latitude", anchor="w", width=10)
-        longitude_label = Tkinter.Label(left_frame, text="Longitude", anchor="w", width=10)
-
-        city_picker = Tkinter.OptionMenu(right_frame, variable, "one", "two", "three")
-        county_entry = Tkinter.Entry(right_frame, width=40)
-        latitude_entry = Tkinter.Entry(right_frame, width=40)
-        longitude_entry = Tkinter.Entry(right_frame, width=40)
-
-        city_label.pack()
-        county_label.pack()
-        latitude_label.pack()
-        longitude_label.pack()
-        city_picker.pack()
-        county_entry.pack()
-        latitude_entry.pack()
-        longitude_entry.pack()
+        city_label.grid(row=0, column=0, sticky=W)
+        county_label.grid(row=1, column=0, sticky=W)
+        latitude_label.grid(row=2, column=0, sticky=W)
+        longitude_label.grid(row=3, column=0, sticky=W)
+        city_picker.grid(row=0, column=1, sticky=EW)
+        county_value.grid(row=1, column=1)
+        latitude_value.grid(row=2, column=1)
+        longitude_values.grid(row=3, column=1)
 
 
-root = Tkinter.Tk()
-ci = CityInformation(root)
+root = Tk()
+ci = CityInformation(root, "ca.json")
 root.mainloop()
