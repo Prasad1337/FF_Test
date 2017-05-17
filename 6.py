64ddf30248f492409dc095bd5962a7a7615f627d
Prasad <prasad.kamat@build.com>
2017-05-17 00:20:25 -0700

    completed question 6

--- 6.py
+++ 6.py
@@ -15,9 +15,22 @@ class CityInformation:
             options.append(x['name'])
         options.sort()
 
-        variable = StringVar(master)
-        variable.set(options[0])  # default value
-        city_picker = apply(OptionMenu, (master, variable) + tuple(options))
+        # Event handler for OptionMenu
+        def picker_update(*args):
+            for x in city_info:
+                if city_picker_variable.get() == x['name']:
+                    county_value.delete(0, END)
+                    latitude_value.delete(0, END)
+                    longitude_value.delete(0, END)
+                    county_value.insert(0, x['full_county_name'])
+                    latitude_value.insert(0, x['primary_latitude'])
+                    longitude_value.insert(0, x['primary_longitude'])
+                    break
+
+        city_picker_variable = StringVar(master)
+        city_picker_variable.set("")  # default value
+        city_picker = apply(OptionMenu, (master, city_picker_variable) + tuple(options))
+        city_picker_variable.trace('w', picker_update)
 
         master.wm_title("City Information")
         city_label = Label(master, text="City")
@@ -26,7 +39,7 @@ class CityInformation:
         longitude_label = Label(master, text="Longitude")
         county_value = Entry(master, width=40)
         latitude_value = Entry(master, width=40)
-        longitude_values = Entry(master, width=40)
+        longitude_value = Entry(master, width=40)
 
         city_label.grid(row=0, column=0, sticky=W)
         county_label.grid(row=1, column=0, sticky=W)
@@ -35,7 +48,7 @@ class CityInformation:
         city_picker.grid(row=0, column=1, sticky=EW)
         county_value.grid(row=1, column=1)
         latitude_value.grid(row=2, column=1)
-        longitude_values.grid(row=3, column=1)
+        longitude_value.grid(row=3, column=1)
 
 
 root = Tk()
